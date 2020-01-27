c5af7568e2a2ffbca11f92e0893372e1c435fa0e
Stephane Bochet <s.bochet1@gmail.com>
2020-01-28 05:29:27 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Map from './components/Leaflet/Map'
+
+import MapPolygons from './components/Leaflet/MapPolygons'
+
+import WuhanVirusData from './data/wuhan-virus.json'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MapPolygons 
+        map_data={WuhanVirusData}
+      
+      />
+    
+    <div style={{height: '300px', width: '300px'}}>
+      <Map 
+        
+      />
+    </div>
     </div>
   );
 }
---
+++ src/components/Leaflet/MapPolygons.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+import {
+  Circle,
+//   CircleMarker,
+  Map,
+//   Polygon,
+//   Polyline,
+  Popup,
+//   Rectangle,
+  TileLayer,
+} from 'react-leaflet'
+
+const center = [51.505, -0.09]
+
+// const polyline = [
+//   [51.505, -0.09],
+//   [51.51, -0.1],
+//   [51.51, -0.12],
+// ]
+
+// const multiPolyline = [
+//   [
+//     [51.5, -0.1],
+//     [51.5, -0.12],
+//     [51.52, -0.12],
+//   ],
+//   [
+//     [51.5, -0.05],
+//     [51.5, -0.06],
+//     [51.52, -0.06],
+//   ],
+// ]
+
+// const polygon = [
+//   [51.515, -0.09],
+//   [51.52, -0.1],
+//   [51.52, -0.12],
+// ]
+
+// const multiPolygon = [
+//   [
+//     [51.51, -0.12],
+//     [51.51, -0.13],
+//     [51.53, -0.13],
+//   ],
+//   [
+//     [51.51, -0.05],
+//     [51.51, -0.07],
+//     [51.53, -0.07],
+//   ],
+// ]
+
+// const rectangle = [
+//   [51.49, -0.08],
+//   [51.5, -0.06],
+// ]
+
+export default class VectorLayersExample extends Component {
+  render() {
+    return (
+      <Map center={center} zoom={3}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {this.props.map_data.map(item => {
+            console.log(item)
+            return (
+                <Circle center={item.coordinates}  color="red"  fillColor="red" radius={100000}>
+                    <Popup>
+                        <div>
+                            {item.county_name}
+                        </div>
+                        <div>
+                            {item.cases}
+                        </div>
+                        {item.country_name}
+
+                    </Popup>
+                </Circle>
+            )
+        })}
+        {/* 
+        <Circle center={[30.583332, 114.283333]} fillColor="blue" radius={100000}>
+            <Popup>Popup in CircleMarker</Popup>
+        </Circle> */}
+        
+        {/* <CircleMarker center={[51.51, -0.12]} color="red" radius={20}>
+          <Popup>Popup in CircleMarker</Popup>
+        </CircleMarker> */}
+        {/* <Polyline color="lime" positions={polyline} />
+        <Polyline color="lime" positions={multiPolyline} />
+        <Polygon color="purple" positions={polygon} />
+        <Polygon color="purple" positions={multiPolygon} />
+        <Rectangle bounds={rectangle} color="black" /> */}
+      </Map>
+    )
+  }
+}
